df6b34bddb90d6c5c03702dcf6652c86b0bccf52
Sailesh Muvvala <muvvalasailesh6@gmail.com>
2025-06-23 18:47:27 +0530

    Implementation stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,8 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
+
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -64,3 +66,6 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
